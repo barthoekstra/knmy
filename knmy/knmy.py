3653534ba6a2d34e0c44a0bd4408561af2555ca3
Bart Hoekstra <barthoekstra@gmail.com>
2022-08-15 17:56:45 +0200

    Update version

--- knmy/knmy.py
+++ knmy/knmy.py
@@ -5,7 +5,7 @@ import datetime
 from knmy.parser import parse_raw_rain_data, parse_raw_weather_data
 
 __title__ = 'knmy'
-__version__ = '1.0.0'
+__version__ = '1.5.1'
 __author__ = 'Bart Hoekstra'
 __license__ = 'MIT'
 
